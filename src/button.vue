3d8881afcc43c9bece8322f84445871aa9c0c3e4
huzhiwu <1627983849@qq.com>
2020-04-25 12:48:07 +0800

    使用iconfont的symbol图标，添加icon组件

--- src/button.vue
+++ src/button.vue
@@ -2,23 +2,25 @@
     <button class="g-button">按钮</button>
 </template>
 <script>
-    export default {
-    }
+    export default {}
 </script>
 <style lang="scss">
-    .g-button{
+    .g-button {
         font-size: var(--font-size);
-        padding:0.5em 1em;
-        border-radius:0.4em;
-        border:1px solid var(--border-color);
+        padding: 0.5em 1em;
+        border-radius: 0.4em;
+        border: 1px solid var(--border-color);
         background-color: var(--button-bg);
-        &:hover{
-            border-color:var(--border-hover-color)
+
+        &:hover {
+            border-color: var(--border-hover-color)
         }
-        &:active{
-            background-color:var(--button-active-bg)
+
+        &:active {
+            background-color: var(--button-active-bg)
         }
-        &:focus{
+
+        &:focus {
             outline: none;
         }
     }
---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href=`i-${name}`></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "icon",
+        props:['name']
+    }
+</script>
+
+<style scoped lang="scss">
+.icon{
+    width:1em;
+    height:1em;
+}
+</style>
