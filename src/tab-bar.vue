dfc50473d302882835ae653cf25a3d0a8fd0e90d
huzhiwu <1627983849@qq.com>
2020-05-03 11:33:01 +0800

    完善tabbar动画

--- src/tab-bar.vue
+++ src/tab-bar.vue
@@ -34,7 +34,9 @@
         position: absolute;
         bottom:0;
         left:0;
-        height:1px;
+        height:3px;
+        border-radius:1.5px;
         background-color:$border-color ;
+        transition: transform .5s cubic-bezier(0.76, 0.61, 0.24, 1.43)
     }
 </style>
